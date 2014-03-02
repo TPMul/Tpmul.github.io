4a371d2601b1c71898d13048bd83731dad99ae5e
TPMul <tmul.xt@gmail.com>
2014-03-02 10:30:01 -0500

    added number game

---
+++ number-game/js/script.js
@@ -0,0 +1,13 @@
+ var num = Math.round(Math.random()*5);
+    
+  function checkGuess(guess){
+    var isguessright = num == guess;   
+       
+       if(isguessright) {
+       alert('You Guessed Right, it was ' + num + " Congrats! Now let's try a new number.");
+       num = Math.round(Math.random()*5)}
+       
+       else{ 
+         alert('Try Again...');
+       }
+      } 
