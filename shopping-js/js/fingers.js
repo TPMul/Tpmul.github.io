1ec8b6ec6c12a50239e1ee03e4160d220dc32ed6
TPMul <tmul.xt@gmail.com>
2014-03-30 11:34:55 -0400

    added the store with js folder

---
+++ shopping-js/js/fingers.js
@@ -0,0 +1,22 @@
+var Fingers = {
+
+  viking: {
+    title: "Minnesota Vikings &#35;1 NFL Finger",
+    price: 24.99,
+    image: "images/viking-finger.png",
+    sizes: ['14"','16"','18"','20"']
+    description:  "Show your Vikings spirit this season with a new addition to our collection! Printed on both sides to get the maximum effect! This hand is available in all three of our heights, and one size fits all hands, small to Viking-sized!",
+    color: "Vikings Purple"},
+    upload: "no"
+
+  customFinger: {
+    title: "Fully Customizable Foam Finger",
+    price: 14.99,
+    image: "images/cust-finger.png",
+    sizes: ['14"','16"','18"','20"']
+    colors: ["black","blue","green","salmon","oj","yellow","purple","pink","white","blue2","green2","salmon2","oj2","yellow2","purple2","pink2"]
+    description:  "This foam hand is fully customizable, perfect for all teams, put your child's team logo on the back, or use it for corperate outings and sport events. No matter who you need to cheer for this hand will get them pumped up!"
+    upload: "yes"},
+
+
+};
