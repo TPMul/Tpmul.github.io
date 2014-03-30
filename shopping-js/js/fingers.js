74a89a9f6ae8aa108a98cd181119fed98a2e8d1e
TPMul <tmul.xt@gmail.com>
2014-03-30 15:24:07 -0400

    added in missing '

--- shopping-js/js/fingers.js
+++ shopping-js/js/fingers.js
@@ -6,7 +6,7 @@ var Items = {
     image: "images/viking-finger.png",
     description:  "Show your Vikings spirit this season with a new addition to our collection! Printed on both sides to get the maximum effect! This hand is available in all three of our heights, and one size fits all hands, small to Viking-sized!",
     sizes: ['14"','16"','18"','20"'],
-    colors: ["Vikings Purple"],
+    colors: [],
     upload: "no"},
 
   customFinger: {
@@ -15,7 +15,22 @@ var Items = {
     image: "images/cust-finger.png",
     description:  "This foam hand is fully customizable, perfect for all teams, put your child's team logo on the back, or use it for corperate outings and sport events. No matter who you need to cheer for this hand will get them pumped up!",
     sizes: ['14"','16"','18"','20"'],
-    colors: ["black","blue","green","salmon","oj","yellow","purple","pink","white","blue2","green2","salmon2","oj2","yellow2","purple2","pink2"],
+    colors: [       "<div id='color-black'></div>",
+                    '<div id="color-blue"></div>',
+                    '<div id="color-green"></div>',
+                    '<div id="color-salmon"></div>',
+                    '<div id="color-oj"></div>',
+                    '<div id="color-yellow"></div>',
+                    '<div id="color-purple"></div>',
+                    '<div id="color-pink"></div>',
+                    '<div id="color-white"></div>',
+                    '<div id="color-blue2"></div>',
+                    '<div id="color-green2"></div>',
+                    '<div id="color-salmon2"></div>',
+                    '<div id="color-oj2"></div>',
+                    '<div id="color-yellow2"></div>',
+                    '<div id="color-purple2"></div>',
+                    '<div id="color-pink2"></div>'],
     upload: "yes"}
 
 
