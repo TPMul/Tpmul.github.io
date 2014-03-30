c2b3984655025add064863ae8659c883344660c5
TPMul <tmul.xt@gmail.com>
2014-03-30 15:46:55 -0400

    removed the Hard coded sizes

--- shopping-js/js/fingers.js
+++ shopping-js/js/fingers.js
@@ -5,7 +5,11 @@ var Items = {
     price: 24.99,
     image: "images/viking-finger.png",
     description:  "Show your Vikings spirit this season with a new addition to our collection! Printed on both sides to get the maximum effect! This hand is available in all three of our heights, and one size fits all hands, small to Viking-sized!",
-    sizes: ['14"','16"','18"','20"'],
+    sizes: [        "<h4 class='detail-headers'>Sizes</h4>"
+                    "<a href='#'><div class='inch-boxes'>14\"</div></a>"
+                    "<a href='#'><div class='inch-boxes'>16\"</div></a>"
+                    "<a href='#'><div class='inch-boxes'>18\"</div></a>"
+                    "<a href='#'><div class='inch-boxes'>20\"</div></a>]",
     colors: [],
     upload: ""},
 
@@ -14,8 +18,14 @@ var Items = {
     price: 14.99,
     image: "images/cust-finger.png",
     description:  "This foam hand is fully customizable, perfect for all teams, put your child's team logo on the back, or use it for corperate outings and sport events. No matter who you need to cheer for this hand will get them pumped up!",
-    sizes: ['14"','16"','18"','20"'],
-    colors: [       "<div id='color-black'></div>",
+    sizes: ["<h4 class='detail-headers'>Sizes</h4>"
+                    "<a href='#'><div class='inch-boxes'>14\"</div></a>"
+                    "<a href='#'><div class='inch-boxes'>16\"</div></a>"
+                    "<a href='#'><div class='inch-boxes'>18\"</div></a>"
+                    "<a href='#'><div class='inch-boxes'>20\"</div></a>]"],
+                    
+    colors: [       "<h4>Choose your color</h4>",
+                    "<div id='color-black'></div>",
                     '<div id="color-blue"></div>',
                     '<div id="color-green"></div>',
                     '<div id="color-salmon"></div>',
