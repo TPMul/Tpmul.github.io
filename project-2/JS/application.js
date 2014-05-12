602f5618395acf4557eeec031e98baff46027602
TPMul <tmul.xt@gmail.com>
2014-05-12 09:24:35 -0400

    trying a link now

--- project-2/JS/application.js
+++ project-2/JS/application.js
@@ -52,7 +52,7 @@
               				   div .html("<img src='"+ d.avatar_url + "'alt='user avatar'>" + "<br>" + "<h3>"+ d.username + "</h3>" + "<br>" + "has " + d.followers_count + " people following them" + "<br>" + "has drafted " + d.draftees_count + " Draftees." + "<br>" + "has received "+ d.likes_received_count + " likes on their shots." + "<br>" + "And are from " + d.country)
               				   .style("left", (d3.event.pageX) + "px")
               				   .style("top", (d3.event.pageY-100) + "px")
-                          .attr("a","href='"+d.url+"'")
+                          .attr("a","href='d.url+'")
                        })  
 
              .on("mouseout", function(d) { div.transition()
